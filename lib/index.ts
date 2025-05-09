e8290d40e5108548370d63b4125304794425ee6b
Smbat1505 <smbat1584@gmail.com>
2025-05-09 13:39:47 +0400

    SCRUM-33 chore: update index.ts

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,4 @@
 import './styles/index.scss'
 // export * from './assets/icons/components' // !!! Uncomment if you want to export icons
-// export * from './components'
+export * from './components'
 export { clsx } from 'clsx'
