657d9a51e61094bc62efd7c870d91bc8536e2b2f
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-05-19 11:16:44 +0300

    fixed bugs

--- lib/components/atoms/Select-box/SelectItem/SelectItems.tsx
+++ lib/components/atoms/Select-box/SelectItem/SelectItems.tsx
@@ -3,34 +3,33 @@ import * as RadixSelect from "@radix-ui/react-select";
 import stl from "./SelectItems.module.scss";
 import { clsx } from "clsx";
 
-import {type ComponentPropsWithoutRef, type ComponentRef, forwardRef, type ReactNode} from "react";
-import { Typography } from "../../../index";
+import { type ComponentPropsWithoutRef, type ComponentRef, forwardRef, type ReactNode } from "react";
 
 type Props = {
-  className?: string;
-  value: string;
-  children?: ReactNode;
+    className?: string;
+    value: string;
+    children?: ReactNode;
 } & ComponentPropsWithoutRef<typeof RadixSelect.Item>;
 
 export const SelectItem = forwardRef<ComponentRef<typeof RadixSelect.Item>, Props>(
-  ({ value, children, disabled, className, ...rest }, ref) => {
-    return (
-      <Typography variant={"regular_16"} color={"light"}>
-        <RadixSelect.Item
-          className={clsx(stl.itemWrapper, className)}
-          value={value}
-          disabled={disabled}
-          ref={ref}
-          {...rest}
-        >
-          <RadixSelect.ItemText>{children}</RadixSelect.ItemText>
-          <RadixSelect.ItemIndicator className={stl.ItemIndicator}>
-            <CheckIcon />
-          </RadixSelect.ItemIndicator>
-        </RadixSelect.Item>
-      </Typography>
-    );
-  }
+    ({ value, children, disabled, className, ...rest }, ref) => {
+        return (
+            <RadixSelect.Item
+                className={clsx(stl.itemWrapper, className)}
+                value={value}
+                disabled={disabled}
+                ref={ref}
+                {...rest}
+            >
+                <RadixSelect.ItemText asChild>
+                    {children}
+                </RadixSelect.ItemText>
+                <RadixSelect.ItemIndicator className={stl.ItemIndicator}>
+                    <CheckIcon />
+                </RadixSelect.ItemIndicator>
+            </RadixSelect.Item>
+        );
+    }
 );
 
 SelectItem.displayName = "SelectItem";
