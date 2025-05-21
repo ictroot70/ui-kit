292e3993b5200c8ab3096971c46cf10b45c8aeaa
Aliaksei Krasko <alekskrasko1985@gmail.com>
2025-05-21 18:23:23 +0300

    SCRUM-28 <added width and padding adaptation and added SelectWithFlag SelectWithShow in storybook>

--- lib/components/molecules/Select-box/Select.stories.tsx
+++ lib/components/molecules/Select-box/Select.stories.tsx
@@ -1,5 +1,7 @@
 import type {Meta, StoryObj} from "@storybook/react";
 import { Select } from "./Select";
+import { RussiaFlag } from '../../../assets/icons'
+import UkFlag from '../../../assets/icons/components/UkFlag'
 
 
 const meta: Meta<typeof Select> = {
@@ -48,5 +50,35 @@ export const SelectDisabled: Story = {
       { value: "TypeScript", label: "TypeScript" },
     ],
     disabled: true,
+  },
+};
+
+
+export const SelectWithFlag: Story = {
+  args: {
+    label: "Выберите язык",
+    defaultValue: "ru",
+    items: [
+      { value: "ru", label: "Русский", icon: <RussiaFlag /> },
+      { value: "en", label: "Английский", icon: <span><UkFlag /></span> },
+    ],
+    disabled: false,
+  },
+};
+
+
+export const SelectWithShow: Story = {
+  args: {
+    defaultValue: "100",
+    style: { width: '50px', padding: "6px" },
+    items: [
+      { value: "10", label: "10" },
+      { value: "20", label: "20" },
+      { value: "30", label: "30" },
+      { value: "50", label: "50" },
+      { value: "100", label: "100" },
+
+    ],
+    disabled: false,
   },
 };
