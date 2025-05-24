8413f0a22e768e9c65ac74f764bf73a05fd3bbc3
Smbat1505 <smbat1584@gmail.com>
2025-05-24 21:46:28 +0400

    SCRUM-43 refactor(ErrorMessage): rename props and class for consistency
             - Renamed the `errorMessage` prop to `message` for simplicity and clarity
             - Renamed the `errorMessage` CSS class to `message` to better reflect its general styling purpose
    
             These changes help make the component more generic and reusable across different contexts, not just for errors.

---
+++ lib/components/atoms/ErrorMessage/ErrorMessage.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { ErrorMessage } from 'components/atoms/ErrorMessage/ErrorMessage'
+
+const meta = {
+  component: ErrorMessage,
+  tags: ['autodocs'],
+  title: 'Components/ErrorMessage',
+  argTypes: {
+    message: {
+      control: 'text',
+    },
+  },
+} satisfies Meta<typeof ErrorMessage>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Small: Story = {
+  args: {
+    errorMessage: 'This is an error message',
+  },
+}
+
+export const Big: Story = {
+  args: {
+    ...Small.args,
+    variant: 'danger',
+  },
+}
