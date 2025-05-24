ebf3718d5e7296fe7a92109d2c3c8c4d4f362eac
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2025-05-25 01:25:32 +0300

    SCRUM-22 feat: implement InteractiveSidebar with SidebarGroup and SidebarLink components for enhanced navigation

--- lib/components/organisms/Sidebar/Sidebar.stories.tsx
+++ lib/components/organisms/Sidebar/Sidebar.stories.tsx
@@ -1,18 +1,23 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { useState } from 'react'
 import {
+  Bookmark,
   BookmarkOutline,
   Home,
   HomeOutline,
   LogOut,
+  MessageCircle,
   MessageCircleOutline,
+  Person,
   PersonOutline,
   PlusSquare,
   PlusSquareOutline,
   Search,
   TrendingUp,
 } from 'assets/icons'
-import styles from './Sidebar.module.scss'
-import { Sidebar, SidebarItem } from './Sidebar'
+import { Sidebar } from './Sidebar'
+import { SidebarGroup } from './SidebarGroup'
+import { SidebarLink } from './SidebarLink'
 
 const meta = {
   component: Sidebar,
@@ -22,69 +27,123 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
+const linkGroups = [
+  {
+    links: [
+      {
+        href: '/feed',
+        icon: <HomeOutline />,
+        activeIcon: <Home />,
+        label: 'Feed',
+      },
+      {
+        href: '/create',
+        icon: <PlusSquareOutline />,
+        activeIcon: <PlusSquare />,
+        label: 'Create',
+      },
+      {
+        href: '/profile',
+        icon: <PersonOutline />,
+        activeIcon: <Person />,
+        label: 'My Profile',
+      },
+      {
+        href: '/messenger',
+        icon: <MessageCircleOutline />,
+        activeIcon: <MessageCircle />,
+        label: 'Messenger',
+      },
+      {
+        href: '/search',
+        icon: <Search />,
+        activeIcon: <Search />,
+        label: 'Search',
+      },
+    ],
+  },
+  {
+    links: [
+      {
+        href: '/statistics',
+        icon: <TrendingUp />,
+        activeIcon: <TrendingUp />,
+        label: 'Statistics',
+        disabled: true,
+      },
+      {
+        href: '/favorites',
+        icon: <BookmarkOutline />,
+        activeIcon: <Bookmark />,
+        label: 'Favorites',
+      },
+    ],
+  },
+  {
+    links: [
+      {
+        href: '/logout',
+        icon: <LogOut />,
+        activeIcon: <LogOut />,
+        label: 'Log Out',
+      },
+    ],
+  },
+]
+
+const InteractiveSidebar = () => {
+  const [activeLink, setActiveLink] = useState('/feed')
+
+  const handleClick = (href: string, disabled?: boolean) => (e: React.MouseEvent) => {
+    e.preventDefault()
+    if (!disabled) {
+      setActiveLink(href)
+    }
+  }
+
+  return (
+    <>
+      {linkGroups.map((group, index) => (
+        <SidebarGroup key={index}>
+          {group.links.map(link => (
+            <SidebarLink
+              key={link.href}
+              href={link.href}
+              icon={link.icon}
+              activeIcon={link.activeIcon}
+              onClick={handleClick(link.href, link.disabled)}
+              active={activeLink === link.href}
+              disabled={link.disabled}
+            >
+              {link.label}
+            </SidebarLink>
+          ))}
+        </SidebarGroup>
+      ))}
+    </>
+  )
+}
+
 export const Default: Story = {
   args: {
     width: 220,
-    children: (
-      <>
-        <div className={styles.group}>
-          <SidebarItem as="a" href="/feed" icon={<HomeOutline />} activeIcon={<Home />} active>
-            Feed
-          </SidebarItem>
-          <SidebarItem as="a" href="/create" icon={<PlusSquareOutline />}>
-            Create
-          </SidebarItem>
-          <SidebarItem as="a" href="/profile" icon={<PersonOutline />}>
-            My Profile
-          </SidebarItem>
-          <SidebarItem as="a" href="/messenger" icon={<MessageCircleOutline />}>
-            Messenger
-          </SidebarItem>
-          <SidebarItem as="a" href="/search" icon={<Search />}>
-            Search
-          </SidebarItem>
-        </div>
-        <div className={styles.group}>
-          <SidebarItem as="a" href="/statistics" icon={<TrendingUp />} disabled>
-            Statistics
-          </SidebarItem>
-          <SidebarItem as="a" href="/favorites" icon={<BookmarkOutline />}>
-            Favorites
-          </SidebarItem>
-        </div>
-        <div className={styles.group}>
-          <SidebarItem as="a" href="/logout" icon={<LogOut />}>
-            Log Out
-          </SidebarItem>
-        </div>
-      </>
-    ),
+    children: <InteractiveSidebar />,
   },
 }
 
-export const AllSidebarItemStates: Story = {
+export const WithScroll: Story = {
   args: {
-    ...Default.args,
-    children: (
-      <>
-        <div className={styles.group}>
-          <SidebarItem as="a" href="/feed" icon={<HomeOutline />} activeIcon={<Home />}>
-            Feed
-          </SidebarItem>
-          <SidebarItem
-            as="a"
-            href="/create"
-            icon={<PlusSquareOutline />}
-            activeIcon={<PlusSquare />}
-            active
-          >
-            Create
-          </SidebarItem>
-          <SidebarItem as="a" href="/profile" icon={<PersonOutline />} disabled>
-            My Profile
-          </SidebarItem>
-        </div>
-      </>
-    ),
+    width: 220,
+    children: <InteractiveSidebar />,
   },
+  parameters: {
+    viewport: {},
+  },
+  decorators: [
+    Story => (
+      <div style={{ height: '400px', border: '1px solid var(--color-primary-500)' }}>
+        <Story />
+      </div>
+    ),
+  ],
 }
