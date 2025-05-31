fed7f21a69636dea3d216b91f7301999b095dd8b
kenesaryLastKing <koni9812@gmail.com>
2025-05-31 09:35:16 +0500

    false

--- lib/components/atoms/RadioGroup/RadioGroup.stories.tsx
+++ lib/components/atoms/RadioGroup/RadioGroup.stories.tsx
@@ -1,17 +1,49 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import React from 'react'
+import type { Meta } from '@storybook/react'
 import RadioGroup from './RadioGroup'
+import styles from './RadioGroup.module.scss'
 
 const meta: Meta<typeof RadioGroup> = {
   title: 'Components/RadioGroup',
   component: RadioGroup,
-  parameters: {
-    layout: 'centered',
-  },
 }
 
 export default meta
-type Story = StoryObj<typeof RadioGroup>
 
-export const Default: Story = {
-  render: () => <RadioGroup />,
+export const AllStates = () => {
+  const values = ['default', 'comfortable', 'compact']
+
+  return (
+    <div className={styles['radio-group-showcase']}>
+      {/* Default */}
+      <div className={styles['state-column']}>
+        <span>Default</span>
+        <RadioGroup value="default" onValueChange={() => {}} />
+      </div>
+
+      {/* Active */}
+      <div className={`${styles['state-column']} ${styles['state-active']}`}>
+        <span>Active</span>
+        <RadioGroup value="comfortable" onValueChange={() => {}} />
+      </div>
+
+      {/* Hover */}
+      <div className={`${styles['state-column']} ${styles['state-hover']}`}>
+        <span>Hover</span>
+        <RadioGroup value="compact" onValueChange={() => {}} />
+      </div>
+
+      {/* Focus */}
+      <div className={`${styles['state-column']} ${styles['state-focus']}`}>
+        <span>Focus</span>
+        <RadioGroup value="default" onValueChange={() => {}} />
+      </div>
+
+      {/* Disabled */}
+      <div className={`${styles['state-column']} ${styles['state-disabled']}`}>
+        <span>Disabled</span>
+        <RadioGroup value="default" onValueChange={() => {}} />
+      </div>
+    </div>
+  )
 }
--- lib/components/atoms/RadioGroup/RadioGroup.tsx
+++ lib/components/atoms/RadioGroup/RadioGroup.tsx
@@ -1,22 +1,40 @@
 import * as RadixRadioGroup from '@radix-ui/react-radio-group'
-import './RadioGroup.scss'
+import './RadixRadioGroup.module.scss'
+import './styles.css'
+
+import './styles.css'
 
 const RadioGroup = () => (
-  <form className="container">
-    <RadixRadioGroup.Root className="radio-root" defaultValue="default" aria-label="View density">
-      {['default', 'comfortable', 'compact'].map((value, i) => (
-        <div className="radio-wrapper" key={value}>
-          <RadixRadioGroup.Item className="radio-item" value={value} id={`r${i + 1}`}>
-            <div className="circle">
-              <div className="dot" />
-            </div>
-            <RadixRadioGroup.Indicator className="radio-indicator" />
-          </RadixRadioGroup.Item>
-          <label className="radio-label" htmlFor={`r${i + 1}`}>
-            {value.charAt(0).toUpperCase() + value.slice(1)}
-          </label>
-        </div>
-      ))}
+  <form>
+    <RadixRadioGroup.Root
+      className="RadixRadioGroupRoot"
+      defaultValue="default"
+      aria-label="View density"
+    >
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <RadixRadioGroup.Item className="RadixRadixRadioGroupItem" value="default" id="r1">
+          <RadixRadioGroup.Indicator className="RadixRadixRadioGroupIndicator" />
+        </RadixRadioGroup.Item>
+        <label className="Label" htmlFor="r1">
+          Default
+        </label>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <RadixRadioGroup.Item className="RadixRadixRadioGroupItem" value="comfortable" id="r2">
+          <RadixRadioGroup.Indicator className="RadixRadixRadioGroupIndicator" />
+        </RadixRadioGroup.Item>
+        <label className="Label" htmlFor="r2">
+          Comfortable
+        </label>
+      </div>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <RadixRadioGroup.Item className="RadixRadixRadioGroupItem" value="compact" id="r3">
+          <RadixRadioGroup.Indicator className="RadixRadixRadioGroupIndicator" />
+        </RadixRadioGroup.Item>
+        <label className="Label" htmlFor="r3">
+          Compact
+        </label>
+      </div>
     </RadixRadioGroup.Root>
   </form>
 )
