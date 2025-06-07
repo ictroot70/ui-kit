25fbe4e9328e6a42c8a53fe616b0e9945ede030d
Aliaksei Krasko <159188011+AliakseiKrasko@users.noreply.github.com>
2025-06-07 20:48:53 +0300

    Scrum 28 select box (#10)
    * SCRUM-28 <Create component select-box and structure folder>
    
    * SCRUM-28 <adde styles>
    
    * SCRUM-28 <added index.ts>
    
    * SCRUM-28 <fixed bags>
    
    * fixed bugs
    
    * change folder
    
    * SCRUM-28 <added icons and hovers styles>
    
    * SCRUM-39 feate: add LabelRadix component
    
    * SCRUM-28 <fixed bugs>
    
    * SCRUM-39 feate: add LabelRadix component
    
    * SCRUM-28 <added LabelRadix and comments>
    
    * SCRUM-28 <added width and padding adaptation and added SelectWithFlag SelectWithShow in storybook>
    
    * SCRUM-28 <added child to RadixSelect>
    
    * SCRUM-28 <fixed bugs>
    
    * SCRUM-28 <fixed bugs styles>
    
    ---------
    
    Co-authored-by: Smbat1505 <smbat1584@gmail.com>

---
+++ lib/components/molecules/Select-box/Select.stories.tsx
@@ -0,0 +1,84 @@
+import type {Meta, StoryObj} from "@storybook/react";
+import { Select } from "./Select";
+import { RussiaFlag } from '../../../assets/icons'
+import UkFlag from '../../../assets/icons/components/UkFlag'
+
+
+const meta: Meta<typeof Select> = {
+  title: "Components/Select",
+  component: Select,
+  tags: ["autodocs"],
+  argTypes: {
+    items: {
+      control: { type: "object" },
+    },
+    disabled: { control: "boolean" },
+  },
+  args: {
+    defaultValue: "React",
+  },
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof Select>;
+
+export default meta;
+type Story = StoryObj<typeof Select>;
+
+export const SelectLanguages: Story = {
+  args: {
+    label: "Select-box",
+    items: [
+      { value: "HTML", label: "HTML" },
+      { value: "CSS", label: "CSS" },
+      { value: "React", label: "React" },
+      { value: "Redux", label: "Redux" },
+      { value: "TypeScript", label: "TypeScript" },
+    ],
+    disabled: false,
+  },
+};
+
+export const SelectDisabled: Story = {
+  args: {
+    label: "select skills",
+    items: [
+      { value: "HTML", label: "HTML" },
+      { value: "CSS", label: "CSS" },
+      { value: "React", label: "React" },
+      { value: "Redux", label: "Redux" },
+      { value: "TypeScript", label: "TypeScript" },
+    ],
+    disabled: true,
+  },
+};
+
+
+export const SelectWithFlag: Story = {
+  args: {
+    label: "Выберите язык",
+    defaultValue: "ru",
+    items: [
+      { value: "ru", label: "Русский", icon: <RussiaFlag /> },
+      { value: "en", label: "Английский", icon: <UkFlag /> },
+    ],
+    disabled: false,
+  },
+};
+
+
+export const SelectWithShow: Story = {
+  args: {
+    defaultValue: "100",
+    style: { width: '70px' },
+    items: [
+      { value: "10", label: "10" },
+      { value: "20", label: "20" },
+      { value: "30", label: "30" },
+      { value: "50", label: "50" },
+      { value: "100", label: "100" },
+
+    ],
+    disabled: false,
+  },
+};
