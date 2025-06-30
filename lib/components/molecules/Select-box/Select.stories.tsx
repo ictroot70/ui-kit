c3e6452605e5585853a165a1b9241c387cdac681
Aliaksei Krasko <159188011+AliakseiKrasko@users.noreply.github.com>
2025-06-30 10:10:42 +0300

    Scrum 28 select box (#40)
    * SCRUM-28 <Create component select-box and structure folder>
    
    * SCRUM-28 <adde styles>
    
    * SCRUM-28 <added index.ts>
    
    * SCRUM-28 <fixed bags>
    
    * fixed bugs
    
    * change folder
    
    * SCRUM-28 <added icons and hovers styles>
    
    * SCRUM-39 feate: add LabelRadix component
    
    * SCRUM-28 <fixed bugs>
    
    * SCRUM-39 feate: add LabelRadix component
    
    * SCRUM-28 <added LabelRadix and comments>
    
    * SCRUM-28 <added width and padding adaptation and added SelectWithFlag SelectWithShow in storybook>
    
    * SCRUM-28 <added child to RadixSelect>
    
    * SCRUM-28 <fixed bugs>
    
    * SCRUM-28 <fixed bugs styles>
    
    * SCRUM-28 <fixed bugs styles select>
    
    * SCRUM-28 <fixed bugs styles select>
    
    * SCRUM-28 <fixed bugs styles select>
    
    * SCRUM-28 <fixed bugs all component select>
    
    ---------
    
    Co-authored-by: Smbat1505 <smbat1584@gmail.com>

--- lib/components/molecules/Select-box/Select.stories.tsx
+++ lib/components/molecules/Select-box/Select.stories.tsx
@@ -1,85 +1,77 @@
-import type { Meta, StoryObj } from '@storybook/react'
-
+import type {Meta, StoryObj} from "@storybook/react";
+import { Select } from "./Select";
 import { RussiaFlag } from '../../../assets/icons'
 import UkFlag from '../../../assets/icons/components/UkFlag'
-import { Select } from './Select'
+import s from './Select.module.scss'
+
 
 const meta: Meta<typeof Select> = {
-  title: 'Components/Select',
+  title: "Components/Select",
   component: Select,
-  tags: ['autodocs'],
+  tags: ["autodocs"],
   argTypes: {
     items: {
-      control: { type: 'object' },
+      control: { type: "object" },
     },
-    disabled: { control: 'boolean' },
+    disabled: { control: "boolean" },
   },
   args: {
-    defaultValue: 'React',
+    defaultValue: "React",
   },
   parameters: {
-    layout: 'centered',
+    layout: "centered",
   },
-} satisfies Meta<typeof Select>
+} satisfies Meta<typeof Select>;
 
-export default meta
-type Story = StoryObj<typeof Select>
+export default meta;
+type Story = StoryObj<typeof Select>;
 
 export const SelectLanguages: Story = {
-  args: {
-    id: 'select-languages',
-    label: 'Select-box',
-    items: [
-      { value: 'HTML', label: 'HTML' },
-      { value: 'CSS', label: 'CSS' },
-      { value: 'React', label: 'React' },
-      { value: 'Redux', label: 'Redux' },
-      { value: 'TypeScript', label: 'TypeScript' },
-    ],
-    disabled: false,
-  },
-}
+  render: () => {
+      return <div style={{width: '210px'}}><Select label={"Select-box"} placeholder={'React'} items={[
+            { value: "HTML", label: "HTML" },
+            { value: "CSS", label: "CSS" },
+            { value: "React", label: "React" },
+            { value: "Redux", label: "Redux" },
+            { value: "TypeScript", label: "TypeScript" },
+          ]}></Select></div>
+    },
+};
 
 export const SelectDisabled: Story = {
-  args: {
-    id: 'select-skills',
-    label: 'select skills',
-    items: [
-      { value: 'HTML', label: 'HTML' },
-      { value: 'CSS', label: 'CSS' },
-      { value: 'React', label: 'React' },
-      { value: 'Redux', label: 'Redux' },
-      { value: 'TypeScript', label: 'TypeScript' },
-    ],
-    disabled: true,
+  render: () => {
+    return <div style={{width: '210px'}}><Select label={"Select-box"} placeholder={'React'} disabled={true} items={[
+      { value: "HTML", label: "HTML" },
+      { value: "CSS", label: "CSS" },
+      { value: "React", label: "React" },
+      { value: "Redux", label: "Redux" },
+      { value: "TypeScript", label: "TypeScript" },
+    ]}></Select></div>
   },
-}
+};
+
 
 export const SelectWithFlag: Story = {
   args: {
-    id: 'select-language',
-    label: 'Выберите язык',
-    defaultValue: 'ru',
+    label: "Выберите язык",
+    defaultValue: "ru",
     items: [
-      { value: 'ru', label: 'Русский', icon: <RussiaFlag /> },
-      { value: 'en', label: 'Английский', icon: <UkFlag /> },
+      { value: "ru", label: "Русский", icon: <RussiaFlag /> },
+      { value: "en", label: "Английский", icon: <UkFlag /> },
     ],
     disabled: false,
   },
-}
+};
 
-export const SelectWithShow: Story = {
-  args: {
-    id: 'select-show',
-    defaultValue: '100',
-    style: { width: '70px' },
-    items: [
-      { value: '10', label: '10' },
-      { value: '20', label: '20' },
-      { value: '30', label: '30' },
-      { value: '50', label: '50' },
-      { value: '100', label: '100' },
-    ],
-    disabled: false,
-  },
-}
+
+export const SelectMedium: Story = {
+  render: () => {
+    return <div className={s.pagination}><Select width={'50px'} placeholder={'100'} pagination items={[
+      { value: "10", label: "10" },
+      { value: "20", label: "20" },
+      { value: "30", label: "30" },
+      { value: "50", label: "50" },
+      { value: "100", label: "100" }
+    ]}></Select></div>
+  }
+};
